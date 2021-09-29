2a4cbd17eb0f577e5b11f5d6124d4e55d9f3e297
JMAGIC_LEARNER <MaJason93@gmail.com>
2021-09-29 00:15:22 -0700

    Adding fixed package json

---
+++ New folder/README.md
@@ -0,0 +1,2 @@
+# Dynamic-Portfolio
+A redux version of my portfolio. I opted to keep my original portfolio to display serverside capabilties, while using this version to display how React manages to render front end HTML
---
+++ New folder/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
+import Project from './components/Project';
+import Header from './components/Header';
+
+function App() {
+    return (
+    <div>
+        
+    <Header/>
+    <Navigation/>
+    <Project/>
+    <Footer/> 
+    </div>
+    );
+}
+
+export default App;
