4c6b79e9903e27c3a45bffba9f78b687cb554b1d
JasonDesktop <MaJason93@gmail.com>
2021-10-13 23:53:46 -0700

    trying to get this to work

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import topPic from '../images/JasonMaBanner.jpg';
 import "../styles/portfolio_styling.css";
+import "../styles/slideshow.css";
 
 function Banner() {
 
     return (
         <div>
     <div className="slideshow-gimmick">
-            <div className="main-image">
-        <img src={topPic} id="slideshow-display" alt="An image of an totally unassuming dude."/>
-            </div>
+           
         <a className="prev" id="prevButton">&#10094;</a>
         <a className="next" id="nextButton">&#10095;</a>
     </div>
 
-   
+    <div className="main-image">
+        <img src={topPic} id="slideshow-display" alt="An image of an totally unassuming dude."/>
+            </div>
     <script type="text/javascript" src="../scripts/slideshow.js"></script>
+    <script type="text/javascript" src="../scripts/slideshow2.js"></script>
+    
     
     </div>
     );
